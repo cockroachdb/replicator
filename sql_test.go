9fa1dba4b18e5fd5880fce87fc59dec2162281a2
Bram Gruneir <bram@cockroachlabs.com>
2020-04-06 16:10:10 -0400

    add sql test

---
+++ sql_test.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+// These test require an insecure cockroach server is running on the default
+// port with the default root user with no password.
+
+func TestGetPrimaryKeyColumns(t *testing.T) {
+	// Create the test db
+	db, dbName, dbClose := getDB(t)
+	defer dbClose()
+
+	testcases := []struct {
+		tableSchema string
+		primaryKeys []string
+	}{
+		{
+			"a INT",
+			[]string{"rowid"},
+		},
+		{
+			"a INT PRIMARY KEY",
+			[]string{"a"},
+		},
+		{
+			"a INT, b INT, PRIMARY KEY (a,b)",
+			[]string{"a", "b"},
+		},
+		{
+			"a INT, b INT, PRIMARY KEY (b,a)",
+			[]string{"b", "a"},
+		},
+		{
+			"a INT, b INT, c INT, PRIMARY KEY (b,a,c)",
+			[]string{"b", "a", "c"},
+		},
+	}
+
+	for i, test := range testcases {
+		t.Run(fmt.Sprintf("%d:%s", i, test.tableSchema), func(t *testing.T) {
+			tableFullName := fmt.Sprintf("%s.test_%d", dbName, i)
+			if _, err := db.Exec(
+				fmt.Sprintf(`CREATE TABLE %s ( %s )`, tableFullName, test.tableSchema),
+			); err != nil {
+				t.Fatal(err)
+			}
+			columns, err := GetPrimaryKeyColumns(db, tableFullName)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !reflect.DeepEqual(test.primaryKeys, columns) {
+				t.Errorf("expected %v, got %v", test.primaryKeys, columns)
+			}
+		})
+	}
+
+}
