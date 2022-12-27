7118de1becb0df6354d54cff560ff29218e3ad87
Bob Vawter <bob@vawter.org>
2022-12-27 12:20:50 -0500

    stdpool: Allow concurrent registration of the same host
    This patch changes the construction of the pool metrics so that the same
    database host can be registered multiple times. This fixes a testing problem
    where some tests need to open multiple pools to the same destintation cluster.

---
+++ internal/util/stdpool/metrics_test.go
@@ -0,0 +1,35 @@
+// Copyright 2022 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package stdpool_test
+
+import (
+	"testing"
+
+	"github.com/cockroachdb/cdc-sink/internal/target/sinktest"
+	"github.com/cockroachdb/cdc-sink/internal/util/stdpool"
+	"github.com/stretchr/testify/require"
+)
+
+// This test verifies that PublishMetrics can be called with the same
+// destination host multiple times without panicking.
+func TestConcurrentRegistration(t *testing.T) {
+	r := require.New(t)
+
+	fixture, cancel, err := sinktest.NewBaseFixture()
+	r.NoError(err)
+	defer cancel()
+
+	c1 := stdpool.PublishMetrics(fixture.Pool)
+	defer c1()
+
+	c2 := stdpool.PublishMetrics(fixture.Pool)
+	defer c2()
+}
