32aa02d34cc4950a3b3ed77182d40c1efe149167
Silvano Ravotto <silvano@cockroachlabs.com>
2023-06-06 09:26:37 -0400

    Support for v23.1 CDC Queries for webhook and http storage based sinks.
    This commit allow cdc-sink users to leverage change data capture queries (https://www.cockroachlabs.com/docs/v23.1/cdc-queries).
    
    In order to provide to cdc-sink with the information required to process the data emitted by the changefeed, the CREATE CHANGEFEED definition must follow these conventions:
    
    . The URL in the must contain the name of the table.
    . The SELECT Statement must return a  "__event__" column that contains the result of the event_op() function.

---
+++ internal/source/cdc/ndjson_query.go
@@ -0,0 +1,109 @@
+// Copyright 2023 The Cockroach Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package cdc
+
+// This file contains code repackaged from url.go.
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"net/url"
+	"regexp"
+
+	"github.com/cockroachdb/cdc-sink/internal/types"
+	"github.com/cockroachdb/cdc-sink/internal/util/ident"
+	"github.com/pkg/errors"
+)
+
+// See https://www.cockroachlabs.com/docs/stable/create-changefeed.html#general-file-format
+// Example: /targetDB/targetSchema/targetTable/2020-04-02/202004022058072107140000000000000-56087568dba1e6b8-1-72-00000000-test_table-1.ndjson
+// Format is: /[endpoint]/[date]/[timestamp]-[uniquer]-[topic]-[schema-id]
+var (
+	ndjsonQueryRegex        = regexp.MustCompile(`^/(?P<targetDB>[^/]+)/(?P<targetSchema>[^/]+)/(?P<targetTable>[^/]+)/(?P<date>\d{4}-\d{2}-\d{2})/(?P<uniquer>.+)-(?P<topic>[^-]+)-(?P<schema_id>[^-]+).ndjson$`)
+	ndjsonQueryTargetDB     = ndjsonQueryRegex.SubexpIndex("targetDB")
+	ndjsonQueryTargetSchema = ndjsonQueryRegex.SubexpIndex("targetSchema")
+	ndjsonQueryTargetTable  = ndjsonQueryRegex.SubexpIndex("targetTable")
+)
+
+func (h *Handler) parseNdjsonQueryURL(url *url.URL, req *request) error {
+	match := ndjsonQueryRegex.FindStringSubmatch(url.Path)
+	if match == nil {
+		return errors.Errorf("can't parse url %s", url)
+	}
+	// for CDC queries, we rely on table from the URL Path, for consistency with webhook
+	table := ident.NewTable(
+		ident.New(match[ndjsonQueryTargetDB]),
+		ident.New(match[ndjsonQueryTargetSchema]),
+		ident.New(match[ndjsonQueryTargetTable]),
+	)
+	req.leaf = func(ctx context.Context, req *request) error {
+		return h.ndjson(ctx, req, h.parseNdjsonQueryMutation)
+	}
+	req.target = table
+	return nil
+
+}
+
+// parseNdjsonQueryMutation is a parseMutation function.
+// When using CDC queries the SELECT Statement must include the event (as "__event__")
+// returned by the event_op() function.
+// SELECT *, event_op() as operation
+// See (https://www.cockroachlabs.com/docs/stable/cdc-queries.html#cdc-query-function-support)
+func (h *Handler) parseNdjsonQueryMutation(
+	ctx context.Context, req *request, rawBytes []byte,
+) (types.Mutation, error) {
+	keys, err := h.getPrimaryKey(ctx, req)
+	if err != nil {
+		return types.Mutation{}, err
+	}
+	qp := queryPayload{
+		keys: keys,
+	}
+	dec := json.NewDecoder(bytes.NewReader(rawBytes))
+	if err := dec.Decode(&qp); err != nil {
+		return types.Mutation{}, err
+	}
+	return qp.AsMutation()
+}
+
+// getPrimaryKey returns a map that contains all the columns that make up the primary key
+// for the target table and their ordinal position within the key.
+func (h *Handler) getPrimaryKey(ctx context.Context, req *request) (map[ident.Ident]int, error) {
+	if req.keys != nil {
+		return req.keys, nil
+	}
+	table, ok := req.target.(ident.Table)
+	if !ok {
+		return nil, errors.Errorf("expecting ident.Table, got %T", req.target)
+	}
+	req.keys = make(map[ident.Ident]int)
+	watcher, err := h.Resolvers.watchers.Get(ctx, table.Database())
+	if err != nil {
+		return nil, err
+	}
+	columns, ok := watcher.Get().Columns[table]
+	if !ok {
+		return nil, errors.Errorf("table %q not found", table)
+	}
+	for i, col := range columns {
+		if col.Primary {
+			req.keys[col.Name] = i
+		}
+	}
+	return req.keys, nil
+}
---
+++ internal/source/cdc/query_payload.go
@@ -0,0 +1,158 @@
+// Copyright 2023 The Cockroach Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package cdc
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+
+	"github.com/cockroachdb/cdc-sink/internal/types"
+	"github.com/cockroachdb/cdc-sink/internal/util/hlc"
+	"github.com/cockroachdb/cdc-sink/internal/util/ident"
+	"github.com/pkg/errors"
+)
+
+const (
+	// JSON operation values
+	deleteFieldValue = `"delete"`
+	insertFieldValue = `"insert"`
+	updateFieldValue = `"update"`
+
+	// labels
+	crdbLabel  = "__crdb__"
+	eventLabel = "__event__"
+)
+
+// Metadata contains a string representation of a timestamp,
+// either as "Resolved" or "Updated".
+type Metadata struct {
+	Resolved string `json:"resolved"`
+	Updated  string `json:"updated"`
+}
+
+// operationType is the type of the operation associated with the
+// mutation: delete,insert or update.
+type operationType int
+
+const (
+	unknownOp operationType = iota
+	deleteOp
+	insertOp
+	updateOp
+)
+
+//go:generate go run golang.org/x/tools/cmd/stringer -type=operationType
+
+// decodeOp reads the operation type from a serialized JSON value.
+func decodeOp(op json.RawMessage) (operationType, error) {
+	switch string(op) {
+	case deleteFieldValue:
+		return deleteOp, nil
+	case insertFieldValue:
+		return insertOp, nil
+	case updateFieldValue:
+		return updateOp, nil
+	}
+	return unknownOp, fmt.Errorf("unknown operation %s", op)
+}
+
+// decodeUpdatedTimestamp extracts the update hlc.Time from a serialized Metadata JSON object.
+func decodeUpdatedTimestamp(data json.RawMessage) (hlc.Time, error) {
+	dec := json.NewDecoder(bytes.NewReader(data))
+	var time Metadata
+	if err := dec.Decode(&time); err != nil {
+		return hlc.Time{}, err
+	}
+	return hlc.Parse(time.Updated)
+}
+
+// queryPayload stores the payload sent by the client for
+// a change feed that uses a query
+type queryPayload struct {
+	after     map[string]json.RawMessage
+	keys      map[ident.Ident]int
+	keyValues []json.RawMessage
+	operation operationType
+	updated   hlc.Time
+}
+
+// AsMutation converts the QueryPayload into a types.Mutation
+func (q *queryPayload) AsMutation() (types.Mutation, error) {
+	var after json.RawMessage
+	if q.operation != deleteOp {
+		var err error
+		after, err = json.Marshal(q.after)
+		if err != nil {
+			return types.Mutation{}, err
+		}
+	}
+	key, err := json.Marshal(q.keyValues)
+	if err != nil {
+		return types.Mutation{}, err
+	}
+	return types.Mutation{
+		Data: after,
+		Key:  key,
+		Time: q.updated,
+	}, nil
+}
+
+// UnmarshalJSON reads a serialized JSON object,
+// and extracts the updated timestamp, the operation, and values
+// for all the remaining fields.
+// If QueryPayload is initialized with Keys that are expected in the data,
+// UnmarshalJSON will extract and store them in keyValues slice.
+// Example:
+// {"__event__": "insert", "pk" : 42, "v" : 9, "__crdb__": {"updated": "1.0"}}
+func (q *queryPayload) UnmarshalJSON(data []byte) error {
+	dec := json.NewDecoder(bytes.NewReader(data))
+	q.keyValues = make([]json.RawMessage, len(q.keys))
+	q.after = make(map[string]json.RawMessage)
+	if err := dec.Decode(&q.after); err != nil {
+		return err
+	}
+	var v json.RawMessage
+	var ok bool
+	var err error
+	// Process eventLabel.
+	if v, ok = q.after[eventLabel]; !ok {
+		return errors.Errorf("CREATE CHANGEFEED must specify the %s colum set to op_event()", eventLabel)
+	}
+	if q.operation, err = decodeOp(v); err != nil {
+		return fmt.Errorf("unable to decode operation type: %w", err)
+	}
+	delete(q.after, eventLabel)
+
+	// Process crdbLabel - it must contain the updated timestamp.
+	if v, ok = q.after[crdbLabel]; !ok {
+		return errors.Errorf("missing %s field", crdbLabel)
+	}
+	if q.updated, err = decodeUpdatedTimestamp(v); err != nil {
+		return err
+	}
+	delete(q.after, crdbLabel)
+
+	// Process keys.
+	for k, pos := range q.keys {
+		if v, ok = q.after[k.Raw()]; !ok {
+			return fmt.Errorf("expecting a value for key: %s", k)
+		}
+		q.keyValues[pos] = v
+	}
+	return nil
+}
