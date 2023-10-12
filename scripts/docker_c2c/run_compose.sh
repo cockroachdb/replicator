e0b940a22a26a052f9779bb74d035cc8001e1cce
glennfawcett <fawcettglenn@gmail.com>
2023-10-12 11:14:22 -0700

    scripts: docker build and doc minus license data
    This change adds a demo based on docker-compose. It will set up a
    Cockroach-to-Cockroach configuration with prometheus and grafana containers.

---
+++ scripts/docker_c2c/run_compose.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Copyright 2023 The Cockroach Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+
+#export COCKROACH_DEV_LICENSE=""
+#export COCKROACH_DEV_ORGANIZATION=""
+
+if [ -z "COCKROACH_DEV_LICENSE" ] || [ -z "COCKROACH_DEV_ORGANIZATION" ]
+then
+     echo "The COCKROACH_DEV_LICENSE and COCKROACH_DEV_ORGANIZATION env variables must be set to run CRDB Changefeeds needed for cdc-sink"
+     exit
+fi
+
+echo "export COCKROACH_DEV_LICENSE='"$COCKROACH_DEV_LICENSE"'" > scripts/crdb_env
+echo "export COCKROACH_DEV_ORGANIZATION='"$COCKROACH_DEV_ORGANIZATION"'" >> scripts/crdb_env
+
+docker-compose up --detach 
+
+echo "Resetting Grafana Admin Password...."
+echo ""
+export cid=`docker ps |grep grafana- |awk '{printf("%s",$1)}'`
+
+echo "docker exec -it ${cid} /usr/share/grafana/bin/grafana cli --homepath /usr/share/grafana admin reset-admin-password cdc-sink"
+docker exec -it ${cid} /usr/share/grafana/bin/grafana cli --homepath "/usr/share/grafana" admin reset-admin-password cdc-sink
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+  echo "Grafana login is:  admin/cdc-sink"
+else
+  echo "Grafana password was not reset. Must do it manually... (admin/admin) is the initial password"
+fi
+
+echo "Grafana URL: http://localhost:3000/login"
---
+++ scripts/docker_c2c/scripts/setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Copyright 2023 The Cockroach Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+
+. ./crdb_env
+
+cockroach sql --insecure --url 'postgresql://root@roach_target:26257/?sslmode=disable' --insecure --execute 'create database if not exists _cdc_sink' 
+
+cockroach sql --url 'postgresql://root@roach_source:26257/?sslmode=disable' --insecure --execute "SET CLUSTER SETTING enterprise.license = '${COCKROACH_DEV_LICENSE}';"
+cockroach sql --url 'postgresql://root@roach_source:26257/?sslmode=disable' --insecure --execute "SET CLUSTER SETTING cluster.organization = '${COCKROACH_DEV_ORGANIZATION}';"
+cockroach sql --url 'postgresql://root@roach_source:26257/?sslmode=disable' --insecure --execute 'SET CLUSTER SETTING kv.rangefeed.enabled = true;'
+
+cockroach workload init movr 'postgresql://root@roach_source:26257/?sslmode=disable'
+cockroach sql --url 'postgresql://root@roach_source:26257/?sslmode=disable' --insecure --execute "backup database movr into 'nodelocal://1/backup/movr';"
+cockroach sql --url 'postgresql://root@roach_target:26257/?sslmode=disable' --insecure --execute " restore database movr from latest in 'nodelocal://1/backup/movr';"
+
