4fadf2336674260a8a9137df89902f1ed3a19eab
Silvano Ravotto <silvano@cockroachlabs.com>
2023-10-12 21:18:27 -0400

    mysql: conditional templates, completing test coverage for target.
    Adding the template to support conditional update flow (compare-and-set,
    deadlines).
    
    Adding template test data for mysql.

---
+++ internal/target/apply/testdata/my/deadline.upsert.sql
@@ -0,0 +1,11 @@
+INSERT
+INTO "schema"."table"("pk0","pk1","val0","val1","has_default")
+WITH data AS (
+  SELECT ? AS "pk0",? AS "pk1",? AS "val0",? AS "val1",CASE WHEN ? THEN ? ELSE expr() END AS "has_default"
+  UNION SELECT ? AS "pk0",? AS "pk1",? AS "val0",? AS "val1",CASE WHEN ? THEN ? ELSE expr() END AS "has_default"
+),
+deadlined AS (SELECT * FROM data WHERE("val0"> now()- INTERVAL '3600' SECOND)AND("val1"> now()- INTERVAL '1' SECOND))
+SELECT * FROM deadlined
+
+ON DUPLICATE KEY UPDATE 
+"val0"=VALUES("val0"),"val1"=VALUES("val1"),"has_default"=VALUES("has_default")
