dedea5266bfb5d961115fbb5e896666e6368809a
Silvano Ravotto <silvano@cockroachlabs.com>
2024-05-24 17:18:55 -0400

    objstore: bucket reader with providers for S3 and local storage.
    This change introduces an abstraction layer to retrieve files from cloud providers.
    The main use case is listing entries in a bucket and retrieving content for a specific
    object. Listing supports options to define the maximum number of entries to retrieve, as
    well as a starting point to start the listing from.
    
    It also provides an initial implementation of the providers that support S3 and
    local storage. The former is backed by the minio library. The latter is mainly
    used for testing purposes.
    
    A simple test suite package is included to verify the basic functionality of each
    provider.  The initial verification is done via mock implementations.
    This should be expanded in the future to cover integration tests.

---
+++ internal/source/objstore/providers/local/local.go
@@ -0,0 +1,91 @@
+// Copyright 2024 The Cockroach Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+// Package local provide access to local storage.
+package local
+
+import (
+	"errors"
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/cockroachdb/field-eng-powertools/stopper"
+	"github.com/cockroachdb/replicator/internal/source/objstore/bucket"
+)
+
+// Config specifies the parameters required to create a bucket reader.
+type Config struct {
+	Directory string // Root directory
+}
+
+// New creates a bucket reader for a local filesystem.
+func New(config *Config) (bucket.Reader, error) {
+	return &localBucket{
+		filesystem: os.DirFS(config.Directory),
+	}, nil
+}
+
+// localBucket is a bucket backed by a filesystem.
+type localBucket struct {
+	filesystem fs.FS
+}
+
+var _ bucket.Reader = &localBucket{}
+
+// Iter implements bucket.Reader
+func (b *localBucket) Walk(
+	ctx *stopper.Context,
+	dir string,
+	options *bucket.WalkOptions,
+	f func(*stopper.Context, string) error,
+) error {
+	count := 0
+	dir = filepath.Clean(dir)
+	return fs.WalkDir(b.filesystem, dir,
+		func(path string, d fs.DirEntry, err error) error {
+			if options.StartAfter != "" && strings.Compare(path, options.StartAfter) <= 0 {
+				return nil
+			}
+			if options.Limit > 0 && count >= options.Limit {
+				return fs.SkipAll
+			}
+			if d != nil && d.IsDir() {
+				if options.Recursive {
+					return nil
+				}
+				return fs.SkipDir
+			}
+			count++
+			return f(ctx, path)
+		})
+}
+
+// Open implements bucket.Reader
+func (b *localBucket) Open(ctx *stopper.Context, file string) (io.ReadCloser, error) {
+	r, err := b.fs().Open(filepath.Clean(file))
+	if errors.Is(err, fs.ErrNotExist) {
+		return nil, errors.Join(bucket.ErrNoSuchKey, err)
+	}
+	return r, err
+}
+
+// fs returns the underlying filesystem.
+func (b *localBucket) fs() fs.FS {
+	return b.filesystem
+}
